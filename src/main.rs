c9085aca1b7398c9e165c63c17d039f997a870cd
Ian Clester <ijc@ijc8.me>
2024-01-25 13:53:23 -0500

    Exercise 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::{fs::File, io::Write};
+
+fn show_info() {
+    eprintln!("MUSI-6106 Assignment Executable");
+    eprintln!("(c) 2024 Stephen Garrett & Ian Clester");
+}
+
 fn main() {
-    println!("Hello, world!");
+   show_info();
+
+    // Parse command line arguments
+    // First argument is input .wav file, second argument is output text file.
+    let args: Vec<String> = std::env::args().collect();
+    // TODO: your code here
+
+    // Open the input wave file and determine number of channels
+    // TODO: your code here; see `hound::WavReader::open`.
+
+    // Read audio data and write it to the output text file (one column per channel)
+    // TODO: your code here; we suggest using `hound::WavReader::samples`, `File::create`, and `write!`.
+    //       Remember to convert the samples to floating point values and respect the number of channels!
 }
