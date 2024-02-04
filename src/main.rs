251f91f53fbffd30fe3cffe42d36b0df49838e53
venkatKrishnan86 <88517272+venkatKrishnan86@users.noreply.github.com>
2024-02-04 09:39:12 -0500

    Updated exercise 2

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,37 @@ fn show_info() {
 fn main() {
    show_info();
 
+   let mut ring_buffer1 = ring_buffer::RingBuffer::<i16>::new(16);
+   ring_buffer1.push(1);
+   ring_buffer1.push(2);
+   ring_buffer1.push(3);
+   ring_buffer1.push(1);
+   ring_buffer1.push(1);
+   ring_buffer1.push(15);
+   ring_buffer1.push(13);
+   ring_buffer1.push(1);
+   ring_buffer1.push(-21);
+   ring_buffer1.push(13);
+   ring_buffer1.push(-1);
+   ring_buffer1.push(13);
+   ring_buffer1.push(4);
+   ring_buffer1.push(1);
+   ring_buffer1.push(2);
+   ring_buffer1.push(3);
+   ring_buffer1.push(5); // Will not happen
+   println!("{}", ring_buffer1.len());
+   println!("{}", ring_buffer1.pop());
+   println!("{}", ring_buffer1.pop());
+   println!("{}", ring_buffer1.pop());
+   println!("{}", ring_buffer1.pop());
+   println!("{:?}", ring_buffer1);
+   ring_buffer1.push(47);
+   ring_buffer1.push(48);
+   println!("{}", ring_buffer1.pop());
+   println!("{:?}", ring_buffer1);
+
+
+
     // Parse command line arguments
     let args: Vec<String> = std::env::args().collect();
     if args.len() < 3 {
@@ -28,4 +59,5 @@ fn main() {
         let sample = sample.unwrap() as f32 / (1 << 15) as f32;
         write!(out, "{}{}", sample, if i % channels as usize == (channels - 1).into() { "\n" } else { " " }).unwrap();
     }
+    // println!("{}", char::default());
 }
--- src/ring_buffer.rs
+++ src/ring_buffer.rs
@@ -1,64 +1,118 @@
+#[derive(Debug)]
 pub struct RingBuffer<T> {
-    // TODO: fill this in.
+    ring_buffer: Vec<T>,
+    capacity: usize,
+    head: Option<usize>,
+    tail: Option<usize>,
 }
 
 impl<T: Copy + Default> RingBuffer<T> {
     pub fn new(length: usize) -> Self {
         // Create a new RingBuffer with `length` slots and "default" values.
         // Hint: look into `vec!` and the `Default` trait.
-        todo!()
+        Self {
+            ring_buffer: vec![T::default(); length],
+            capacity: length,
+            head: None,
+            tail: None,
+        }
     }
 
     pub fn reset(&mut self) {
         // Clear internal buffer and reset indices.
-        todo!()
+        self.head = None;
+        self.tail = None;
     }
 
     // `put` and `peek` write/read without advancing the indices.
     pub fn put(&mut self, value: T) {
-        todo!()
+        match self.tail {
+            None => (),
+            Some(t) => self.ring_buffer[t] = value
+        }
     }
 
     pub fn peek(&self) -> T {
-        todo!()
+        match self.tail {
+            None => T::default(),
+            Some(t) => self.ring_buffer[t]
+        }
     }
 
     pub fn get(&self, offset: usize) -> T {
-        todo!()
+        match self.tail {
+            None => T::default(),
+            Some(t) => self.ring_buffer[(t+offset)%self.capacity]
+        }
     }
 
     // `push` and `pop` write/read and advance the indices.
     pub fn push(&mut self, value: T) {
-        todo!()
+        match self.len() == self.capacity() {
+            true => (),
+            false => match self.tail {
+                None => {
+                    self.head = Some(0);
+                    self.tail = Some(0);
+                    self.ring_buffer[0] = value;
+                },
+                Some(t) => {
+                    self.tail = Some((t+1)%self.capacity);
+                    self.ring_buffer[self.tail.unwrap() as usize] = value;
+                }
+            }
+        }
+        
     }
 
     pub fn pop(&mut self) -> T {
-        todo!()
+        match self.len() == 0 {
+            true => T::default(),
+            false => {
+                let popped_val = self.ring_buffer[self.head.unwrap()];
+                if self.head.unwrap() == self.tail.unwrap() {
+                    self.head = None;
+                    self.tail = None;
+                } else {
+                    self.head = Some((self.head.unwrap() + 1)%self.capacity);
+                }
+                popped_val
+            }
+        }
     }
 
     pub fn get_read_index(&self) -> usize {
-        todo!()
+        self.head.unwrap()
     }
 
     pub fn set_read_index(&mut self, index: usize) {
-        todo!()
+        self.head = Some(index)
     }
 
     pub fn get_write_index(&self) -> usize {
-        todo!()
+        self.tail.unwrap()
     }
 
     pub fn set_write_index(&mut self, index: usize) {
-        todo!()
+        self.tail = Some(index)
     }
 
     pub fn len(&self) -> usize {
         // Return number of values currently in the buffer.
-        todo!()
+        match (self.head, self.tail) {
+            (Some(h), Some(t)) => {
+                if t >= h {
+                    return t-h+1;
+                } else {
+                    return self.capacity - (h-t-1);
+                }
+            },
+            (_, _) => 0
+        }
     }
 
     pub fn capacity(&self) -> usize {
         // Return the length of the internal buffer.
-        todo!()
+        self.capacity
     }
 }
