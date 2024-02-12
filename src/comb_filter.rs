634360b452cc22eca80336ec934e783c94ccbb2c
venkatKrishnan86 <88517272+venkatKrishnan86@users.noreply.github.com>
2024-02-12 17:51:17 -0500

    Updated the entire combfilter.rs Add the data in every function in the original skeleton

--- src/comb_filter.rs
+++ src/comb_filter.rs
@@ -1,5 +1,13 @@
+use ringbuffer::{AllocRingBuffer, RingBuffer};
+
 pub struct CombFilter {
     // TODO: your code here
+    filter_type: FilterType,
+    delay: f32,
+    delayed_signal_amp_factor: f32,
+    sample_rate_hz: f32,
+    num_channels: usize,
+    delay_line: AllocRingBuffer<f32>
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -21,26 +29,122 @@ pub enum Error {
 
 impl CombFilter {
     pub fn new(filter_type: FilterType, max_delay_secs: f32, sample_rate_hz: f32, num_channels: usize) -> Self {
-        todo!("implement")
+        Self {
+            filter_type: filter_type,
+            delay: max_delay_secs,
+            delayed_signal_amp_factor: 0.5,
+            sample_rate_hz: sample_rate_hz,
+            num_channels: num_channels,
+            delay_line: AllocRingBuffer::with_capacity((max_delay_secs * sample_rate_hz) as usize)
+        }
     }
 
     pub fn reset(&mut self) {
-        todo!("implement")
+        self.delay_line.clear();
     }
 
     pub fn process(&mut self, input: &[&[f32]], output: &mut [&mut [f32]]) {
-        todo!("implement");
+        match self.filter_type {
+            FilterType::FIR => {
+                for (input_block, output_block) in input.iter().zip(output.iter_mut()) {
+                    for (input_sample, output_sample) in input_block.iter().zip(output_block.iter_mut()) {
+                        *output_sample = input_sample + self.delayed_signal_amp_factor * self.delay_line.peek().unwrap_or(&0.0);
+                        self.delay_line.push(*input_sample);
+                    }
+                }
+            },
+            FilterType::IIR => {
+                for (input_block, output_block) in input.iter().zip(output.iter_mut()) {
+                    for (input_sample, output_sample) in input_block.iter().zip(output_block.iter_mut()) {
+                        *output_sample = input_sample + self.delayed_signal_amp_factor * self.delay_line.peek().unwrap();
+                        self.delay_line.push(*output_sample);
+                    }
+                }
+            }
+        }
     }
 
     pub fn set_param(&mut self, param: FilterParam, value: f32) -> Result<(), Error> {
-        todo!("implement")
+        match param {
+            FilterParam::Gain => {
+                if value < 0.0 || value > 1.0 {
+                    return Err(Error::InvalidValue { param: param, value: value });
+                }
+                self.delayed_signal_amp_factor = value
+            },
+            FilterParam::Delay => self.delay = value
+        }
+        Ok(())
     }
 
     pub fn get_param(&self, param: FilterParam) -> f32 {
-        todo!("implement")
+        match param {
+            FilterParam::Gain => self.delayed_signal_amp_factor,
+            FilterParam::Delay => self.delay
+        }
+    }
+
+    pub fn vec_mul(v1: &[f32], v2: &[f32]) -> Vec<f32>
+    {
+        assert_eq!(v1.len(), v2.len());
+        v1.iter().zip(v2).map(|(&i1, &i2)| i1 * i2).collect()
     }
 
     // TODO: feel free to define other functions for your own use
 }
 
 // TODO: feel free to define other types (here or in other modules) for your own use
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[should_panic]
+    #[test]
+    fn setting_wrong_gain_params_for_comb_filter() {
+        let mut filter = CombFilter::new(FilterType::FIR, 0.1, 100.0, 2);
+        let result = filter.set_param(FilterParam::Gain, 2.0);
+        match result {
+            Ok(()) => (),
+            Err(_) => panic!("Error")
+        }
+
+        let result = filter.set_param(FilterParam::Gain, -0.2);
+        match result {
+            Ok(()) => (),
+            Err(_) => panic!("Error")
+        }
+    }
+
+    #[test]
+    fn setting_correct_gain_params_for_comb_filter() {
+        let mut filter = CombFilter::new(FilterType::FIR, 0.1, 100.0, 2);
+        let result = filter.set_param(FilterParam::Gain, 0.2);
+        match result {
+            Ok(()) => (),
+            Err(_) => panic!("Error")
+        }
+    }
+
+    #[test]
+    fn setting_delay_params_for_comb_filter() {
+        let mut filter = CombFilter::new(FilterType::FIR, 0.1, 100.0, 2);
+        let result = filter.set_param(FilterParam::Delay, -1.2);
+        match result {
+            Ok(()) => (),
+            Err(_) => panic!("Error")
+        }
+
+        let result = filter.set_param(FilterParam::Delay, 0.2);
+        match result {
+            Ok(()) => (),
+            Err(_) => panic!("Error")
+        }
+
+        let result = filter.set_param(FilterParam::Delay, 1.2);
+        match result {
+            Ok(()) => (),
+            Err(_) => panic!("Error")
+        }
+    }
+}
