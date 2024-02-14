65be647d38d6d43352ba9fde3162be720c10457d
venkatKrishnan86 <88517272+venkatKrishnan86@users.noreply.github.com>
2024-02-14 10:37:16 -0500

    Converting string .txt writing to a wav audio file writer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::{fs::File, io::Write};
 
+use hound::WavWriter;
 use utils::ProcessBlocks;
+use comb_filter::{CombFilter, FilterType};
 
 mod comb_filter;
 mod utils;
@@ -28,33 +30,28 @@ fn main() {
 
     // TODO: Modify this to process audio in blocks using your comb filter and write the result to an audio file.
     //       Use the following block size:
-    let block_size: usize = 1024;
+    let block_size: usize = 1024*64;
 
     // Read audio data and write it to the output text file (one column per channel)
-    let mut out: File = File::create(&args[2]).expect("Unable to create file");
+    // let out: File = File::create(&args[2]).expect("Unable to create file");
 
-    let mut comb_filter_1 = comb_filter::CombFilter::new(comb_filter::FilterType::FIR, 0.1, 44100.0, channels as usize);
-    let mut channel_output_values: Vec<String> = Vec::new();
-    for _ in 0..channels {
-        channel_output_values.push(String::from(""));
-    }
+    let mut comb_filter_1 = CombFilter::new(FilterType::FIR, 0.1, 44100.0, channels);
+    let mut writer = WavWriter::create(&args[2], spec).expect("Unable to create file");
 
     while let Ok(block) = reader.samples::<i16>().take(block_size*channels as usize).collect::<Result<Vec<_>, _>>() {
         let mut process_block = ProcessBlocks::new(&block, &channels);
-        process_block.convert_i16_samples_to_f32(&block, &channels);
         let (input_address, mut output_address) = process_block.create_and_write_addresses();
 
         comb_filter_1.process(&input_address, &mut output_address);
 
-        for (i, output_block) in process_block.output_block.into_iter().enumerate() {
-            for value in output_block {
-                channel_output_values[i].push_str(&format!("{}\n", value));
+        let output_block: Vec<Vec<f32>> = utils::transpose(process_block.output_block);
+
+        for output in output_block.into_iter() {
+            for value in output {
+                writer.write_sample(utils::f32_to_i16(value)).unwrap();
             }
         }
 
         if block.len() < block_size*channels as usize { break }
-    }
-    for channel_sample in channel_output_values {
-        write!(out, "{}", channel_sample).unwrap();
     }
 }
