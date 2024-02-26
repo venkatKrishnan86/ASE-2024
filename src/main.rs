501407d8fcaebf9571f5c043950b6547c740180a
venkatKrishnan86 <88517272+venkatKrishnan86@users.noreply.github.com>
2024-02-26 16:07:28 -0500

    Took the code block from comb filter

--- src/main.rs
+++ src/main.rs
@@ -24,10 +24,29 @@ fn main() {
     let spec = reader.spec();
     let channels = spec.channels;
 
-    // Read audio data and write it to the output text file (one column per channel)
-    let mut out = File::create(&args[2]).expect("Unable to create file");
-    for (i, sample) in reader.samples::<i16>().enumerate() {
-        let sample = sample.unwrap() as f32 / (1 << 15) as f32;
-        write!(out, "{}{}", sample, if i % channels as usize == (channels - 1).into() { "\n" } else { " " }).unwrap();
+    // // Read audio data and write it to the output text file (one column per channel)
+    // let mut out = File::create(&args[2]).expect("Unable to create file");
+    // for (i, sample) in reader.samples::<i16>().enumerate() {
+    //     let sample = sample.unwrap() as f32 / (1 << 15) as f32;
+    //     write!(out, "{}{}", sample, if i % channels as usize == (channels - 1).into() { "\n" } else { " " }).unwrap();
+    // }
+    if spec.bits_per_sample!=16 {
+        eprintln!("Bit depth must be 16 bit! Bit depth of the current song: {}", spec.bits_per_sample);
+        return
     }
+
+    // TODO: Modify this to process audio in blocks using your comb filter and write the result to an audio file.
+    //       Use the following block size:
+    let block_size: usize = 1024;
+
+    // vibrato::process_and_write_audio(
+    //     &mut reader, 
+    //     block_size, 
+    //     channels,
+    //     &args[2], 
+    //     spec, 
+    //     comb_filter::FilterType::IIR, 
+    //     0.8,
+    //     0.1
+    // );
 }
