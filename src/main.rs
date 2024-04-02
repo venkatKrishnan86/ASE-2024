957b49369796e91bed9df44f35d08ef15570cbec
venkatKrishnan86 <88517272+venkatKrishnan86@users.noreply.github.com>
2024-04-02 15:23:48 -0400

    Copied the main from assignment-2

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use std::{fs::File, io::BufWriter};
 
 mod ring_buffer;
-mod fast_convolver;
+// mod fast_convolver;
 mod utils;
 
-// use utils::ProcessBlocks;
+use utils::ProcessBlocks;
 
 fn show_info() {
     eprintln!("MUSI-6106 Assignment Executable");
@@ -34,13 +34,13 @@ fn main() {
 
     let block_size: usize = 32;
 
-    // let mut writer: hound::WavWriter<BufWriter<File>> = hound::WavWriter::create(output_file, spec).expect("Unable to create file");
+    let mut writer: hound::WavWriter<BufWriter<File>> = hound::WavWriter::create(output_file, spec).expect("Unable to create file");
 
-    // while let Ok(block) = reader.samples::<i16>().take(block_size*channels).collect::<Result<Vec<_>, _>>() {
-    //     let mut process_block = ProcessBlocks::new(&block, &channels);
-    //     let (input_address, mut output_address) = process_block.create_and_write_addresses();
-    //     // vibrato_filter.process(&input_address, &mut output_address);
-    //     process_block.write_output_samples(&mut writer).unwrap();
-    //     if block.len() < block_size*channels as usize { break }
-    // }
+    while let Ok(block) = reader.samples::<i16>().take(block_size*channels).collect::<Result<Vec<_>, _>>() {
+        let mut process_block = ProcessBlocks::new(&block, &channels);
+        let (input_address, mut output_address) = process_block.create_and_write_addresses();
+        // vibrato_filter.process(&input_address, &mut output_address);
+        process_block.write_output_samples(&mut writer).unwrap();
+        if block.len() < block_size*channels as usize { break }
+    }
 }
