988e624128ad35354fcc821da57788a0337cda6e
venkatKrishnan86 <88517272+venkatKrishnan86@users.noreply.github.com>
2024-04-02 15:15:26 -0400

    Time Domain is implemented

--- src/fast_convolver.rs
+++ src/fast_convolver.rs
@@ -1,5 +1,6 @@
-struct FastConvolver {
-    // TODO: your fields here
+struct FastConvolver<'a> {
+    impulse_response: &'a[f32],
+    mode: ConvolutionMode
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -8,24 +9,40 @@ pub enum ConvolutionMode {
     FrequencyDomain { block_size: usize },
 }
 
-impl FastConvolver {
-    pub fn new(impulse_response: &[f32], mode: ConvolutionMode) -> Self {
-        todo!("implement")
+impl<'a> FastConvolver<'a> {
+    pub fn new(impulse_response: &'a[f32], mode: ConvolutionMode) -> Self {
+        Self {
+            impulse_response: impulse_response,
+            mode: mode
+        }
     }
 
     pub fn reset(&mut self) {
-        todo!("implement")
+        todo!("Implement")
     }
 
     pub fn process(&mut self, input: &[f32], output: &mut [f32]) {
-        todo!("implement")
+        match self.mode {
+            ConvolutionMode::TimeDomain => {
+                self.convolve(input, self.impulse_response, output)
+            },
+            ConvolutionMode::FrequencyDomain { block_size } => {
+
+            }
+        }
     }
 
     pub fn flush(&mut self, output: &mut [f32]) {
         todo!("implement")
     }
 
-    // TODO: feel free to define other functions for your own use
+    pub fn convolve(&mut self, input1: &[f32], input2: &[f32], output: &mut [f32]) {
+        for (idx1, i1) in input1.iter().enumerate() {
+            for (idx2, i2) in input2.iter().enumerate() {
+                output[idx1 + idx2] = i1*i2
+            }
+        }
+    }
 }
 
 // TODO: feel free to define other types (here or in other modules) for your own use
