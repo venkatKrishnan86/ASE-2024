01b6b5748f16d849bb5e0a963f65cd8ca36465ec
venkatKrishnan86 <88517272+venkatKrishnan86@users.noreply.github.com>
2024-04-22 12:38:40 -0400

    Added the appropriate tests

--- src/fast_convolver.rs
+++ src/fast_convolver.rs
@@ -13,14 +13,24 @@ pub struct FastConvolver {
     impulse_response: Vec<f32>,
     block_size: usize,
     mode: ConvolutionMode,
-    fft: Radix4<f32>,
-    ifft: Radix4<f32>
+    fft: Option<Radix4<f32>>,
+    ifft: Option<Radix4<f32>>
 }
 
 impl FastConvolver {
     pub fn new(impulse_response: Vec<f32>, mode: ConvolutionMode, max_block_size: usize) -> Self {
-        let fft = Radix4::new(max_block_size*2, FftDirection::Forward);
-        let ifft = Radix4::new(max_block_size*2, FftDirection::Inverse);
+        let fft: Option<Radix4<f32>>;
+        let ifft: Option<Radix4<f32>>;
+        match mode {
+            ConvolutionMode::TimeDomain => {
+                fft = None;
+                ifft = None;
+            },
+            ConvolutionMode::FrequencyDomain => {
+                fft = Some(Radix4::new(max_block_size*2, FftDirection::Forward));
+                ifft = Some(Radix4::new(max_block_size*2, FftDirection::Inverse));
+            }
+        } 
         Self {
             buffer: vec![0.0; (impulse_response.len()/max_block_size + 2)*max_block_size],
             impulse_response,
@@ -31,12 +41,12 @@ impl FastConvolver {
         }
     }
 
-    pub fn _reset(&mut self) {
+    pub fn reset(&mut self) {
         self.buffer.clear();
         self.impulse_response.clear();
     }
 
-    pub fn _set_block_size(&mut self, block_size: usize){
+    pub fn set_block_size(&mut self, block_size: usize){
         let len_ir = self.impulse_response.len() - 1;
         self.block_size = max(block_size, len_ir);
     }
@@ -58,7 +68,7 @@ impl FastConvolver {
             for (ir_idx, ir_block) in self.impulse_response.chunks(self.block_size).enumerate() {
                 match self.mode {
                     ConvolutionMode::TimeDomain => FastConvolver::time_convolve(input_block, ir_block, output, &mut self.buffer, (main_idx+ir_idx) * self.block_size, output_len),
-                    ConvolutionMode::FrequencyDomain => FastConvolver::frequency_convolve(input_block, ir_block, output, &mut self.buffer, (main_idx+ir_idx) * self.block_size, self.block_size, output_len, &self.fft, &self.ifft),
+                    ConvolutionMode::FrequencyDomain => FastConvolver::frequency_convolve(input_block, ir_block, output, &mut self.buffer, (main_idx+ir_idx) * self.block_size, self.block_size, output_len, self.fft.as_ref().unwrap(), self.ifft.as_ref().unwrap()),
                 };
             }
         }   
@@ -137,9 +147,8 @@ impl FastConvolver {
 #[cfg(test)]
 mod tests {
     use rand::Rng;
-
+    use rustfft::num_traits::Pow;
     use super::*;
-
 
     #[test]
     fn test_identity_impulse_response() {
@@ -163,7 +172,6 @@ mod tests {
         let mut rng = rand::thread_rng();
         let mut impulse_response = vec![0.0; 50];
         impulse_response[0] = 1.0;
-        println!("impulse_response : {:?}", impulse_response);
         let input_signal: Vec<f32> = (0..block_size).map(|_| rng.gen::<f32>()).collect();
         let mut output_signal: Vec<f32> = vec![0.0_f32; block_size];
         let mut convolver = FastConvolver::new(impulse_response, ConvolutionMode::FrequencyDomain, block_size);
@@ -173,11 +181,10 @@ mod tests {
         }
     }
 
-    #[test]
-    fn test_flush() {
-        let block_size = 16;
+    fn test_flush(block_size: usize, mode: ConvolutionMode, ir_length: usize) {
+        // let block_size = 16;
         let mut rng = rand::thread_rng();
-        let mut impulse_response = vec![0.0; 50];
+        let mut impulse_response = vec![0.0; ir_length];
         impulse_response[0] = 1.0;
 
         let gain = rng.gen::<f32>();
@@ -185,7 +192,7 @@ mod tests {
         impulse_response[block_size] = gain; // Will delay the whole input by 10 samples with a random gain
         let input_signal: Vec<f32> = (0..block_size).map(|_| rng.gen::<f32>()).collect();
         let mut output_signal: Vec<f32> = vec![0.0_f32; block_size];
-        let mut convolver = FastConvolver::new(impulse_response, ConvolutionMode::TimeDomain, block_size);
+        let mut convolver = FastConvolver::new(impulse_response, mode, block_size);
         convolver.process(&input_signal, &mut output_signal);
         for (in_val, out_val) in input_signal.iter().zip(output_signal.iter()){
             assert!((out_val - in_val).abs() < 1e-5);
@@ -193,7 +200,6 @@ mod tests {
 
         // The reverb tail is checking if the delay is done correctly
         let tail_len = convolver.get_output_tail_size();
-        println!("{}", tail_len);
         let mut reverb_tail: Vec<f32> = vec![0.0_f32; tail_len];
         convolver.flush(&mut reverb_tail);
         for i in 0..block_size{
@@ -202,37 +208,16 @@ mod tests {
         for i in block_size..tail_len{
             assert!(reverb_tail[i].abs() < 1e-5);                            // Rest of the values need to be 0.0
         }
+    }
+
+    #[test]
+    fn test_flush_time() {
+        test_flush(16, ConvolutionMode::TimeDomain, 50)
     }
 
     #[test]
     fn test_flush_frequency() {
-        let block_size = 16;
-        let mut rng = rand::thread_rng();
-        let mut impulse_response = vec![0.0; 50];
-        impulse_response[0] = 1.0;
-
-        let gain = rng.gen::<f32>();
-
-        impulse_response[block_size] = gain; // Will delay the whole input by 10 samples with a random gain
-        let input_signal: Vec<f32> = (0..block_size).map(|_| rng.gen::<f32>()).collect();
-        let mut output_signal: Vec<f32> = vec![0.0_f32; block_size];
-        let mut convolver = FastConvolver::new(impulse_response, ConvolutionMode::FrequencyDomain, block_size);
-        convolver.process(&input_signal, &mut output_signal);
-        for (in_val, out_val) in input_signal.iter().zip(output_signal.iter()){
-            assert!((out_val - in_val).abs() < 1e-5);
-        }
-
-        // The reverb tail is checking if the delay is done correctly
-        let tail_len = convolver.get_output_tail_size();
-        println!("{}", tail_len);
-        let mut reverb_tail: Vec<f32> = vec![0.0_f32; tail_len];
-        convolver.flush(&mut reverb_tail);
-        for i in 0..block_size{
-            assert!((reverb_tail[i] - input_signal[i]*gain).abs() < 1e-5);   // First 10 tail values must be the delayed input signal times the random gain
-        }
-        for i in block_size..tail_len{
-            assert!(reverb_tail[i].abs() < 1e-5);                            // Rest of the values need to be 0.0
-        }
+        test_flush(16, ConvolutionMode::FrequencyDomain, 50)
     }
 
     #[test]
@@ -251,32 +236,24 @@ mod tests {
         }
     }
 
-//     #[test]
-//     fn test_variable_block_sizes() {
-//         let mut input_signal = vec![0.0_f32; 10000];
-//         input_signal[3] = 1.0;
-//         let mut rng = rand::thread_rng();
-//         let impulse_response: Vec<f32> = (0..51).map(|_| rng.gen::<f32>()).collect();
-//         let block_sizes = vec![1, 13, 1023, 2048, 1, 17, 5000, 1897];
-//         let mut output_signal = vec![0.0_f32; input_signal.len()];
-//         let mut convolver = FastConvolver::new(&impulse_response, ConvolutionMode::TimeDomain, 2);
+    #[test]
+    fn test_variable_block_sizes_time() {
+        let block_sizes = vec![1, 13, 1023, 2048, 1, 17, 5000, 1897];
+        for block_size in block_sizes {
+            test_flush(block_size, ConvolutionMode::TimeDomain, 6000);
+        }
+    }
 
-//         for &block_size in &block_sizes {
-//             let mut start_index = 0;
-//             while start_index < input_signal.len() {
-//                 let end_index = std::cmp::min(start_index + block_size, input_signal.len());
-//                 let block = &input_signal[start_index..end_index];
-//                 let output_block = &mut output_signal[start_index..end_index];
-//                 convolver.process(block, output_block);
-//                 start_index += block_size;
-//             }
-//             convolver.reset();
-//         }
-//         println!("impulse_response : {:?}", impulse_response);
-//         println!("Output Signal with variable block sizes: {:?}", output_signal);
-//         let expected_output_at_3 = impulse_response[3]; // As calculated
-//         println!("Expected Output at Index 3: {}", expected_output_at_3);
-//         println!("Actual Output at Index 3: {}", output_signal[3]);
-//         assert!((output_signal[3] - expected_output_at_3).abs() < 1e-5);
-//     }
+    #[test]
+    fn test_variable_block_sizes_freq() {
+        let base: usize = 2;
+        let block_sizes = (0..12)
+            .into_iter()
+            .map(|value| base.pow(value))
+            .collect::<Vec<usize>>();
+        // block_sizes = [1,2,4,8...,2048]
+        for block_size in block_sizes {
+            test_flush(block_size, ConvolutionMode::FrequencyDomain, 5000);
+        }
+    }
 }
